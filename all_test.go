c4972b3ad1e7c819dda1e1f82580fc752846fb04
Hiromu OCHIAI <otiai10@gmail.com>
2017-09-11 20:58:23 +0900

    (just like) starting over

--- all_test.go
+++ all_test.go
@@ -3,51 +3,26 @@ package cwl
 import (
 	"fmt"
 	"os"
-	"reflect"
 	"testing"
 
 	. "github.com/otiai10/mint"
 )
 
-const cwl_version = "1.0"
+const version = "1.0"
 
+// Provides path for testable official .cwl files.
 func cwl(name string) string {
-	return fmt.Sprintf("./testdata/cwl/v%[1]s/v%[1]s/%s", cwl_version, name)
-}
-
-func TestDecode(t *testing.T) {
-	file, err := os.Open("./testdata/001-1st-tool.yaml")
-	if err != nil {
-		panic(err)
-	}
-	root := NewCWL()
-	Expect(t, root).TypeOf("*cwl.Root")
-	err = root.Decode(file)
-	Expect(t, err).ToBe(nil)
-	Expect(t, root.Version).ToBe("v1.0")
-}
-
-func TestDecodeInputs(t *testing.T) {
-	file, err := os.Open("./testdata/001-inputs.yaml")
-	if err != nil {
-		panic(nil)
-	}
-	inputs := NewInputs()
-	err = inputs.Decode(file)
-	Expect(t, err).ToBe(nil)
-	Expect(t, inputs["message"]).ToBe(ProvidedInput{Self: "Hello world!", Type: reflect.String})
-	Expect(t, inputs["inputfile"].Class).ToBe("File")
-	Expect(t, inputs["inputfile"].Path).ToBe("hogee.txt")
+	return fmt.Sprintf("./cwl/v%[1]s/v%[1]s/%s", version, name)
 }
 
 func TestDecode_bwa_mem_tool(t *testing.T) {
-	file, err := os.Open(cwl("bwa-mem-tool.cwl"))
+	f, err := os.Open(cwl("bwa-mem-tool.cwl"))
 	if err != nil {
 		panic(err)
 	}
 	root := NewCWL()
 	Expect(t, root).TypeOf("*cwl.Root")
-	err = root.Decode(file)
+	err = root.Decode(f)
 	Expect(t, err).ToBe(nil)
 	Expect(t, root.Version).ToBe("v1.0")
 }
--- root.go
+++ root.go
@@ -1,75 +1,44 @@
 package cwl
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
 	"io"
 	"io/ioutil"
-	"sort"
 
-	yaml "gopkg.in/yaml.v2"
+	"github.com/otiai10/yaml2json"
 )
 
 // NewCWL ...
 func NewCWL() *Root {
 	root := new(Root)
-	root.RequiredInputs = map[string]RequiredInput{}
-	root.ProvidedInputs = ProvidedInputs{}
+	// root.RequiredInputs = RequiredInputs{}
+	// root.ProvidedInputs = ProvidedInputs{}
 	return root
 }
 
 // Root ...
 type Root struct {
-	Version        string                   `yaml:"cwlVersion"`
-	Path           string                   `yaml:"-"`
-	Class          string                   `yaml:"class"`
-	BaseCommand    string                   `yaml:"baseCommand"`
-	RequiredInputs map[string]RequiredInput `yaml:"inputs"`
-	ProvidedInputs ProvidedInputs           `yaml:"-"`
+	Version string `json:"cwlVersion"`
+	// Class          string         `json:"class"`
+	// BaseCommand    string         `json:"baseCommand"`
+	// RequiredInputs RequiredInputs `json:"inputs"`
+	// ProvidedInputs ProvidedInputs `json:"-"`
 }
 
-// RequiredInput ...
-type RequiredInput struct {
-	Name    string       `yaml:"-"`
-	Type    string       `yaml:"string"`
-	Binding InputBinding `yaml:"inputBinding"`
-}
-
-// RequiredInputsSortable ...
-type RequiredInputsSortable []RequiredInput
-
-// Len ...
-func (s RequiredInputsSortable) Len() int { return len(s) }
-
-// Swap ...
-func (s RequiredInputsSortable) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
-
-// Less ...
-func (s RequiredInputsSortable) Less(i, j int) bool {
-	return s[i].Binding.Position < s[j].Binding.Position
-}
-
-// InputBinding ...
-type InputBinding struct {
-	Position int `yaml:"position"`
-}
-
-// Args TODO: should be Workflow
-func (root *Root) Args() ([]string, error) {
-	inputs := RequiredInputsSortable{}
-	for key, val := range root.RequiredInputs {
-		if _, ok := root.ProvidedInputs[key]; !ok {
-			return nil, fmt.Errorf("Input `%s` is required but not provided", key)
+// UnmarshalJSON ...
+func (root *Root) UnmarshalJSON(b []byte) error {
+	docs := map[string]interface{}{}
+	if err := json.Unmarshal(b, &docs); err != nil {
+		return err
+	}
+	for key, val := range docs {
+		switch key {
+		case "cwlVersion":
+			root.Version = val.(string)
 		}
-		val.Name = key
-		inputs = append(inputs, val)
 	}
-	sort.Sort(inputs)
-
-	args := []string{}
-	for _, required := range inputs {
-		args = append(args, root.ProvidedInputs[required.Name].Arg())
-	}
-	return args, nil
+	return nil
 }
 
 // Decode decodes specified io.Reader to this root
@@ -78,8 +47,11 @@ func (root *Root) Decode(r io.Reader) error {
 	if err != nil {
 		return err
 	}
-	err = yaml.Unmarshal(buf, root)
+	buf, err = yaml2json.Y2J(bytes.NewReader(buf))
 	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(buf, root); err != nil {
 		return err
 	}
 	return nil
