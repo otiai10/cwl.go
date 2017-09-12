4818d4cf4a340d7f5e8d13b9306759c3814dd762
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-09-12 13:15:01 +0000

    Add test case for `formattest2.cwl`

--- all_test.go
+++ all_test.go
@@ -1042,3 +1042,27 @@ func TestDecode_formattest(t *testing.T) {
 	Expect(t, root.BaseCommands[0]).ToBe("rev")
 	Expect(t, root.Stdout).ToBe("output.txt")
 }
+
+func TestDecode_formattest2(t *testing.T) {
+	f := cwl("formattest2.cwl")
+	root := NewCWL()
+	Expect(t, root).TypeOf("*cwl.Root")
+	err := root.Decode(f)
+	Expect(t, err).ToBe(nil)
+	Expect(t, root.Version).ToBe("v1.0")
+	Expect(t, root.Class).ToBe("CommandLineTool")
+	Expect(t, len(root.Inputs)).ToBe(1)
+	Expect(t, root.Inputs[0].ID).ToBe("input")
+	Expect(t, root.Inputs[0].Types[0].Type).ToBe("File")
+	Expect(t, root.Inputs[0].Format).ToBe("edam:format_2330")
+	// TODO check inputBuinding position default value
+	//Expect(t, root.Inputs[0].Binding.Position).ToBe(1)
+	Expect(t, len(root.Outputs)).ToBe(1)
+	Expect(t, root.Outputs[0].ID).ToBe("output")
+	Expect(t, root.Outputs[0].Types[0].Type).ToBe("File")
+	Expect(t, root.Outputs[0].Binding.Glob).ToBe("output.txt")
+	Expect(t, root.Outputs[0].Format).ToBe("$(inputs.input.format)")
+	Expect(t, len(root.BaseCommands)).ToBe(1)
+	Expect(t, root.BaseCommands[0]).ToBe("rev")
+	Expect(t, root.Stdout).ToBe("output.txt")
+}
