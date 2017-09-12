ec22474b3df9ab66d6a4c5cf0f10c6752e344437
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-09-12 06:17:45 +0000

    Add `env-tool2.cwl`

--- all_test.go
+++ all_test.go
@@ -470,3 +470,26 @@ func TestDecode_cat5_tool(t *testing.T) {
 	Expect(t, len(root.Namespaces)).ToBe(1)
 	Expect(t, root.Namespaces[0]["ex"]).ToBe("http://example.com/")
 }
+
+func TestDecode_env_tool2(t *testing.T) {
+	f := cwl("env-tool2.cwl")
+	root := NewCWL()
+	Expect(t, root).TypeOf("*cwl.Root")
+	err := root.Decode(f)
+	Expect(t, err).ToBe(nil)
+	Expect(t, root.Version).ToBe("v1.0")
+	Expect(t, root.Class).ToBe("CommandLineTool")
+	// TODO in: string support
+	Expect(t, len(root.Outputs)).ToBe(1)
+	Expect(t, root.Outputs[0].ID).ToBe("out")
+	Expect(t, root.Outputs[0].Types[0].Type).ToBe("File")
+	Expect(t, root.Outputs[0].Binding.Glob).ToBe("out")
+	Expect(t, len(root.BaseCommands)).ToBe(3)
+	Expect(t, root.BaseCommands[0]).ToBe("/bin/bash")
+	Expect(t, root.BaseCommands[1]).ToBe("-c")
+	Expect(t, root.BaseCommands[2]).ToBe("echo $TEST_ENV")
+	//
+	Expect(t, root.Hints).TypeOf("cwl.Hints")
+	Expect(t, root.Hints[0]["class"]).ToBe("EnvVarRequirement")
+	Expect(t, root.Hints[0]["envDef"]).ToBe("debian:wheezy")
+}
