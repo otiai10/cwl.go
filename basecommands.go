18a3e9f19dd261da12f555cac627ceea8506b5ad
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-09-12 02:19:45 +0000

    Extends basecommand
    support string and string array

---
+++ basecommands.go
@@ -0,0 +1,27 @@
+package cwl
+
+// BaseCommands ...
+type BaseCommands []string
+
+// New constructs "BaseCommands" struct.
+func (baseCommands BaseCommands) New(i interface{}) BaseCommands {
+	switch x := i.(type) {
+
+	case string:
+		dest := []string{}
+		dest = append(dest, x)
+		return dest
+	case []interface{}:
+		dest := make([]string, len(x))
+		for i, elm := range x {
+			str, ok := elm.(string)
+			if !ok {
+                                return dest
+                        }
+			dest[i] = str
+		}
+		return dest
+	}
+	return BaseCommands{}
+}
+
