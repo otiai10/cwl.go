34a516eb919e3d3278758d21c1e6047005d53e37
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-09-12 08:31:32 +0000

    Update comment

--- envdef.go
+++ envdef.go
@@ -1,12 +1,12 @@
 package cwl
 
-// RequirementEnvDef only appears if requirement class is "InitialWorkDirRequirement"
+// EnvDef only appears if requirement class is "EnvVarRequirement"
 type EnvDef struct {
 	Name  string
 	Value string
 }
 
-// NewList constructs a list of RequirementEntry from interface
+// NewList constructs a list of EnvDef from interface
 func (list EnvDef) NewList(i interface{}) []EnvDef {
 	dest := []EnvDef{}
 	switch x := i.(type) {
