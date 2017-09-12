53c336cbbb46c9e13f339576025d9324ccd774c8
Hiromu OCHIAI <otiai10@gmail.com>
2017-09-12 14:35:13 +0900

    Add InlineJavascriptRequirement and InitialWorkDirRequirement

--- all_test.go
+++ all_test.go
@@ -262,6 +262,37 @@ func TestDecode_cat1_testcli(t *testing.T) {
 	Expect(t, root.Arguments[0]).ToBe("cat")
 }
 
+func TestDecode_template_tool(t *testing.T) {
+	f := cwl("template-tool.cwl")
+	root := NewCWL()
+	err := root.Decode(f)
+	Expect(t, err).ToBe(nil)
+
+	Expect(t, root.Version).ToBe("v1.0")
+	Expect(t, root.Class).ToBe("CommandLineTool")
+
+	Expect(t, root.Requirements[0].Class).ToBe("InlineJavascriptRequirement")
+	Expect(t, root.Requirements[0].ExpressionLib[0].Include).ToBe("underscore.js")
+	Expect(t, root.Requirements[0].ExpressionLib[1].Execute).ToBe("var t = function(s) { return _.template(s)({'inputs': inputs}); };")
+
+	Expect(t, root.Requirements[1].Class).ToBe("InitialWorkDirRequirement")
+	Expect(t, root.Requirements[1].Listing[0].Name).ToBe("foo.txt")
+	Expect(t, root.Requirements[1].Listing[0].Entry).ToBe(`$(t("The file is <%= inputs.file1.path.split('/').slice(-1)[0] %>\n"))`)
+
+	Expect(t, root.Hints[0]["class"]).ToBe("DockerRequirement")
+	Expect(t, root.Hints[0]["dockerPull"]).ToBe("debian:8")
+
+	Expect(t, root.Inputs[0].ID).ToBe("file1")
+	Expect(t, root.Inputs[0].Types[0].Type).ToBe("File")
+
+	Expect(t, root.Outputs[0].ID).ToBe("foo")
+	Expect(t, root.Outputs[0].Types[0].Type).ToBe("File")
+	Expect(t, root.Outputs[0].Binding.Glob).ToBe("foo.txt")
+
+	Expect(t, root.BaseCommands[0]).ToBe("cat")
+	Expect(t, root.BaseCommands[1]).ToBe("foo.txt")
+}
+
 func TestDecode_cat3_nodocker(t *testing.T) {
 	f := cwl("cat3-nodocker.cwl")
 	root := NewCWL()
