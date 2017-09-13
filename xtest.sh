0a8ba6b371ca538938c3e098c1f6e5242f4991d0
Hiromu OCHIAI <otiai10@gmail.com>
2017-09-13 18:43:55 +0900

    Change test framework

--- xtest.sh
+++ xtest.sh
@@ -3,18 +3,30 @@ count=0
 limit=600
 
 function exec_test() {
-    if go test ; then
-        echo "[OK]"
+    if go test -run $1; then
+        echo "[OK: ${1}]"
     else
         if [ $count -gt $limit ]; then
             echo "[FAILED: TIMEOUT!]"
             exit 1
         fi
         count=`expr $count + 1`
-        echo "[NG: TRY AGAIN] ${count}"
+        echo "[NG: TRY AGAIN: ${1}] ${count}"
         echo "-"
-        exec_test
+        exec_test $1
     fi
 }
 
-exec_test
+targets=()
+if [ -n "${1}" ]; then
+  targets+=($1)
+else
+  for filename in `ls xtest_*.go`; do
+    targets+=(`echo xtest_basename_fields_test.go | sed -e s/xtest_// | sed -e s/\.go//`)
+  done
+fi
+
+for t in $targets; do
+  echo $t
+  exec_test $t
+done
