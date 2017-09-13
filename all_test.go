cf497f08bc3f937cf4989af83cc82941643ea0be
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-09-13 07:36:39 +0000

    Add test case `nested-array.cwl`

--- all_test.go
+++ all_test.go
@@ -1421,3 +1421,22 @@ func TestDecode_nameroot(t *testing.T) {
 	Expect(t, root.Arguments[2].Value).ToBe("$(inputs.file1.nameroot)")
 	Expect(t, root.Arguments[3].Value).ToBe("$(inputs.file1.nameext)")
 }
+
+func TestDecode_nested_array(t *testing.T) {
+	f := cwl("nested-array.cwl")
+	root := NewCWL()
+	err := root.Decode(f)
+	Expect(t, err).ToBe(nil)
+	Expect(t, root.Version).ToBe("v1.0")
+	Expect(t, root.Class).ToBe("CommandLineTool")
+	Expect(t, root.BaseCommands[0]).ToBe("echo")
+	Expect(t, len(root.Inputs)).ToBe(1)
+	Expect(t, root.Inputs[0].ID).ToBe("letters")
+	Expect(t, root.Inputs[0].Types[0].Type).ToBe("array")
+	Expect(t, root.Inputs[0].Types[0].Items[0].Type).ToBe("array")
+	Expect(t, root.Inputs[0].Types[0].Items[0].Items[0].Type).ToBe("string")
+	Expect(t, root.Inputs[0].Binding.Position).ToBe(1)
+	Expect(t, root.Stdout).ToBe("echo.txt")
+	Expect(t, root.Outputs[0].ID).ToBe("echo")
+	Expect(t, root.Outputs[0].Types[0].Type).ToBe("stdout")
+}
