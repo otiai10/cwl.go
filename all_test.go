1e69a24297b61882a85a955a71809d0f9cc09fad
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-09-13 03:55:59 +0000

    Add test case `inline-js.cwl`

--- all_test.go
+++ all_test.go
@@ -1324,3 +1324,46 @@ func TestDecode_initialworkdirrequirement_docker_out(t *testing.T) {
 	Expect(t, len(root.BaseCommands)).ToBe(1)
 	Expect(t, root.BaseCommands[0]).ToBe("touch")
 }
+
+func TestDecode_inline_js(t *testing.T) {
+	f := cwl("inline-js.cwl")
+	root := NewCWL()
+	Expect(t, root).TypeOf("*cwl.Root")
+	err := root.Decode(f)
+	Expect(t, err).ToBe(nil)
+	Expect(t, root.Version).ToBe("v1.0")
+	Expect(t, root.Class).ToBe("CommandLineTool")
+	// TODO test BaseCommand because this file has two baseCommand fields
+	//fmt.Println(root.BaseCommands)
+	//Expect(t, len(root.BaseCommands)).ToBe(0)
+	//Expect(t, root.BaseCommands[0]).ToBe("touch")
+	Expect(t, len(root.Requirements)).ToBe(1)
+	Expect(t, root.Requirements[0].Class).ToBe("InlineJavascriptRequirement")
+	Expect(t, len(root.Requirements)).ToBe(1)
+	Expect(t, root.Hints[0].Class).ToBe("DockerRequirement")
+	Expect(t, root.Hints[0].DockerPull).ToBe("python:2-slim")
+	Expect(t, len(root.Inputs)).ToBe(1)
+	Expect(t, root.Inputs[0].ID).ToBe("args.py")
+	Expect(t, root.Inputs[0].Types[0].Type).ToBe("File")
+	Expect(t, root.Inputs[0].Default.Class).ToBe("File")
+	Expect(t, root.Inputs[0].Default.Location).ToBe("args.py")
+	Expect(t, root.Inputs[0].Binding.Position).ToBe(-1)
+	Expect(t, len(root.Outputs)).ToBe(1)
+	Expect(t, root.Outputs[0].ID).ToBe("args")
+	Expect(t, root.Outputs[0].Types[0].Type).ToBe("array")
+	Expect(t, root.Outputs[0].Types[0].Items).ToBe("string")
+	Expect(t, len(root.Arguments)).ToBe(3)
+	Expect(t, root.Arguments[0].CommandLineBinding["prefix"]).ToBe("-A")
+	Expect(t, root.Arguments[0].CommandLineBinding["valueFrom"]).ToBe("$(1+1)")
+	Expect(t, root.Arguments[1].CommandLineBinding["prefix"]).ToBe("-B")
+	Expect(t, root.Arguments[1].CommandLineBinding["valueFrom"]).ToBe(`$("/foo/bar/baz".split('/').slice(-1)[0])`)
+	Expect(t, root.Arguments[2].CommandLineBinding["prefix"]).ToBe("-C")
+	Expect(t, root.Arguments[2].CommandLineBinding["valueFrom"]).ToBe(`${
+  var r = [];
+  for (var i = 10; i >= 1; i--) {
+    r.push(i);
+  }
+  return r;
+}
+`)
+}
