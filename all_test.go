5a7e01a087f233957758863c4144540e964e2b48
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-09-13 09:13:18 +0000

    Add test case `params.cwl`

--- all_test.go
+++ all_test.go
@@ -1485,3 +1485,19 @@ func TestDecode_optional_output(t *testing.T) {
 	Expect(t, root.BaseCommands[0]).ToBe("cat")
 	Expect(t, root.Stdout).ToBe("output.txt")
 }
+
+func TestDecode_params(t *testing.T) {
+	f := cwl("params.cwl")
+	root := NewCWL()
+	err := root.Decode(f)
+	Expect(t, err).ToBe(nil)
+	Expect(t, root.Version).ToBe("v1.0")
+	Expect(t, root.Class).ToBe("CommandLineTool")
+	Expect(t, root.Inputs[0].ID).ToBe("bar")
+	Expect(t, root.Inputs[0].Types[0].Type).ToBe("Any")
+	// TODO test input default
+	// TODO check specification for this test ID and Type
+	Expect(t, root.Outputs[0].ID).ToBe("$import")
+	Expect(t, root.Outputs[0].Types[0].Type).ToBe("params_inc.yml")
+	Expect(t, root.BaseCommands[0]).ToBe("true")
+}
