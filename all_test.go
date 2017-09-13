6554218d437c2efd5b77b34050df7bd4fca1df3c
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-09-13 09:35:52 +0000

    Add test case `rename.cwl`

--- all_test.go
+++ all_test.go
@@ -878,3 +878,26 @@ func TestDecode_params2(t *testing.T) {
 	Expect(t, root.Outputs[0].Types[0].Type).ToBe("params_inc.yml")
 	Expect(t, root.BaseCommands[0]).ToBe("true")
 }
+
+func TestDecode_rename(t *testing.T) {
+	f := cwl("rename.cwl")
+	root := NewCWL()
+	err := root.Decode(f)
+	Expect(t, err).ToBe(nil)
+	Expect(t, root.Version).ToBe("v1.0")
+	Expect(t, root.Class).ToBe("CommandLineTool")
+	Expect(t, root.BaseCommands[0]).ToBe("true")
+	Expect(t, len(root.Requirements)).ToBe(1)
+	Expect(t, root.Requirements[0].Class).ToBe("InitialWorkDirRequirement")
+	Expect(t, root.Requirements[0].Listing[0].EntryName).ToBe("$(inputs.newname)")
+	Expect(t, root.Requirements[0].Listing[0].Entry).ToBe(`$(inputs.srcfile)`)
+	Expect(t, len(root.Inputs)).ToBe(2)
+	Expect(t, root.Inputs[0].ID).ToBe("srcfile")
+	Expect(t, root.Inputs[0].Types[0].Type).ToBe("File")
+	Expect(t, root.Inputs[1].ID).ToBe("newname")
+	Expect(t, root.Inputs[1].Types[0].Type).ToBe("string")
+	Expect(t, len(root.Outputs)).ToBe(1)
+	Expect(t, root.Outputs[0].ID).ToBe("outfile")
+	Expect(t, root.Outputs[0].Types[0].Type).ToBe("File")
+	Expect(t, root.Outputs[0].Binding.Glob[0]).ToBe("$(inputs.newname)")
+}
