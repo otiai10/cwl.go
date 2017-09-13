f9d1d06fa19d7310d6d15d4c166fee6a67b6d89e
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-09-13 17:04:43 +0000

    Split test case

--- all_test.go
+++ all_test.go
@@ -833,20 +833,3 @@ func TestDecode_null_expression1_tool(t *testing.T) {
 	Expect(t, root.Outputs[0].Types[0].Type).ToBe("int")
 	Expect(t, root.Expression).ToBe(`$({'output': (inputs.i1 == 'the-default' ? 1 : 2)})`)
 }
-
-func TestDecode_null_expression2_tool(t *testing.T) {
-	f := cwl("null-expression2-tool.cwl")
-	root := NewCWL()
-	err := root.Decode(f)
-	Expect(t, err).ToBe(nil)
-	Expect(t, root.Version).ToBe("v1.0")
-	Expect(t, root.Class).ToBe("ExpressionTool")
-	Expect(t, len(root.Requirements)).ToBe(1)
-	Expect(t, root.Requirements[0].Class).ToBe("InlineJavascriptRequirement")
-	Expect(t, len(root.Inputs)).ToBe(1)
-	Expect(t, root.Inputs[0].ID).ToBe("i1")
-	Expect(t, root.Inputs[0].Types[0].Type).ToBe("Any")
-	Expect(t, root.Outputs[0].ID).ToBe("output")
-	Expect(t, root.Outputs[0].Types[0].Type).ToBe("int")
-	Expect(t, root.Expression).ToBe(`$({'output': (inputs.i1 == 'the-default' ? 1 : 2)})`)
-}
