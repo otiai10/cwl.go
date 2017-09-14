6604a0ae74572aacec8ecb1b161b014d2b2b6dd4
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-09-14 01:49:42 +0000

    Add test case `wc-tool.cwl`

---
+++ xtest_wc_tool_test.go
@@ -0,0 +1,27 @@
+package cwl
+
+import (
+	"testing"
+
+	. "github.com/otiai10/mint"
+)
+
+func TestDecode_wc_tool(t *testing.T) {
+	f := cwl("wc-tool.cwl")
+	root := NewCWL()
+	err := root.Decode(f)
+	Expect(t, err).ToBe(nil)
+	Expect(t, root.Version).ToBe("v1.0")
+	Expect(t, root.Class).ToBe("CommandLineTool")
+	Expect(t, root.Inputs[0].ID).ToBe("file1")
+	Expect(t, root.Inputs[0].Types[0].Type).ToBe("File")
+	// TODO check specification for this test ID and Type
+	Expect(t, root.Outputs[0].ID).ToBe("output")
+	Expect(t, root.Outputs[0].Types[0].Type).ToBe("File")
+	Expect(t, root.Outputs[0].Binding.Glob[0]).ToBe("output")
+	Expect(t, len(root.BaseCommands)).ToBe(2)
+	Expect(t, root.BaseCommands[0]).ToBe("wc")
+	Expect(t, root.BaseCommands[1]).ToBe("-l")
+	Expect(t, root.Stdin).ToBe("$(inputs.file1.path)")
+	Expect(t, root.Stdout).ToBe("output")
+}
