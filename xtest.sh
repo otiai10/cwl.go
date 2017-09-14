1c6bc8c263fb826b9185c26c2c65112dd46783b7
Hiromu OCHIAI <otiai10@gmail.com>
2017-09-14 20:46:29 +0900

    Refactor test framework

--- xtest.sh
+++ xtest.sh
@@ -1,39 +1,57 @@
+#!/bin/bash
 
+LIMIT_FOR_EACH_CASE=100
+count_for_case=0
 total=0
-count=1
+report=()
 
-limit_for_each=1000
 function exec_test() {
-
-    total=`expr $total + 1`
-
-    target=$1
-
-    if go test -run $1; then
-        echo "[OK: ${count}] $target"
+    total=`expr ${total} + 1`
+    TESTCASE=${1}
+    count_for_case=`expr ${count_for_case} + 1`
+    if go test -run ${TESTCASE} ; then
+        echo "--- OK ${count_for_case}: ${TESTCASE}"
+        report+=("${count_for_case}	${TESTCASE}")
+        count_for_case=0
     else
-        if [ $count -gt ${limit_for_each} ]; then
-            echo "[FAILED: TIMEOUT: ${count} : total ${total}] $target"
+        if [ ${count_for_case} -gt ${LIMIT_FOR_EACH_CASE} ]; then
+            echo ">> FAILED: TIMEOUT: ${count_for_case} times for ${TESTCASE} <<"
             exit 1
         fi
-        echo "[NG: TRY AGAIN: ${count}] ${target}"
-        echo "-"
-        count=`expr $count + 1`
-        exec_test $target
+        echo "--- TRY AGAIN: ${count_for_case}: ${TESTCASE}"
+        exec_test ${TESTCASE}
     fi
 }
 
-targets=()
-if [ -n "${1}" ]; then
-  targets+=($1)
-else
-  for filename in `ls xtest_*.go`; do
+function gather_testcases() {
+  targets=()
+  if [ -n "${1}" ]; then
+    list=`ls xtest_*${1}*_test.go`
+  else
+    list=`ls xtest_*.go`
+  fi
+  for filename in ${list} ; do
     targets+=(`echo ${filename} | sed -e s/xtest_// | sed -e s/_test\.go//`)
   done
-fi
+  echo ${targets[@]}
+}
 
-for t in ${targets[@]} ; do
-  exec_test $t
-done
+function final_report() {
+  IFS=''
+  echo "[COMPLETED]"
+  for r in ${report[@]} ; do
+    echo ${r}
+  done
+  echo "-------------------------"
+  echo "${total}	TOTAL"
+}
 
-echo "[COMPLETED: total ${total} times]"
+function __main__() {
+  testcases=`gather_testcases ${1}`
+  for t in ${testcases[@]} ; do
+    exec_test ${t}
+  done
+  final_report
+}
+
+__main__ $@
