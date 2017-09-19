6640ad02e2d8ca7e82a88a569b3b630af84fa238
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-09-19 08:25:36 +0000

    Split test case `dir4.cwl`

---
+++ tests/dir4_test.go
@@ -0,0 +1,32 @@
+package cwlgotest
+
+import (
+	"testing"
+
+	cwl "github.com/otiai10/cwl.go"
+	. "github.com/otiai10/mint"
+)
+
+func TestDecode_dir4(t *testing.T) {
+	f := load("dir4.cwl")
+	root := cwl.NewCWL()
+	err := root.Decode(f)
+	Expect(t, err).ToBe(nil)
+
+	Expect(t, root.Requirements[0].Class).ToBe("ShellCommandRequirement")
+	Expect(t, root.Inputs[0].ID).ToBe("inf")
+	Expect(t, root.Inputs[0].Types[0].Type).ToBe("File")
+	Expect(t, root.Outputs[0].ID).ToBe("outlist")
+	Expect(t, root.Outputs[0].Types[0].Type).ToBe("File")
+	Expect(t, root.Outputs[0].Binding.Glob).ToBe([]string{"output.txt"})
+	Expect(t, root.Arguments[0].Value).ToBe("cd")
+	Expect(t, root.Arguments[1].Value).ToBe("$(inputs.inf.dirname)/xtestdir")
+	Expect(t, root.Arguments[2].Binding.ShellQuote).ToBe(false)
+	Expect(t, root.Arguments[2].Binding.ValueFrom).ToBe("&&")
+	Expect(t, root.Arguments[3].Value).ToBe("find")
+	Expect(t, root.Arguments[4].Value).ToBe(".")
+	Expect(t, root.Arguments[5].Binding.ShellQuote).ToBe(false)
+	Expect(t, root.Arguments[5].Binding.ValueFrom).ToBe("|")
+	Expect(t, root.Arguments[6].Value).ToBe("sort")
+	Expect(t, root.Stdout).ToBe("output.txt")
+}
